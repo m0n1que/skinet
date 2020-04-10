fac3226498e79c25a75bed89ca171e42aee4f1d9
Monika <jovanova02monika@gmail.com>
2020-04-10 18:00:11 +0200

    End of section 3

--- Infrastucture/Data/Migrations/20200410150419_InitialCreate.Designer.cs
+++ Infrastucture/Data/Migrations/20200410150419_InitialCreate.Designer.cs
@@ -2,14 +2,16 @@
 using Infrastucture.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastucture.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20200410150419_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
